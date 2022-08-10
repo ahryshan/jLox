38c235a246b086236804e133632e466542f70b55
nightingazer <nightingazer@outlook.com>
2022-08-10 13:54:13 +0200

    draft some basic Lox class structure

--- src/com/makinginterpreters/jlox/Lox.java
+++ src/com/makinginterpreters/jlox/Lox.java
@@ -1,10 +1,15 @@
 package com.makinginterpreters.jlox;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Lox {
+    public static boolean hadError = false;
+
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: lox [path/to/script]");
@@ -18,12 +23,35 @@ public class Lox {
         }
     }
 
-    public static void runFile(String path) throws IOException {
+    private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+        if(hadError) System.exit(65);
     }
 
-    public static void runPrompt() throws IOException {
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
 
+        for (; ; ) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null) break;
+            run(line);
+            hadError = false;
+        }
     }
 
+    private static void run(String source) {
+        //TODO actual implementation of this;
+    }
+
+    private static void error(int line, String message) {
+        report(line, "", message);
+        hadError = true;
+    }
+
+    private static void report(int line, String where, String message) {
+        System.out.println("[line " + line + "] Error" + where + ": ");
+    }
 }
